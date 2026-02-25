64e92bf3df368c72b713c9aed19a1a998d2ae235
Donte Truong <dontetruong@gmail.com>
2026-02-25 15:10:15 -0500

    Chart placeholder display

--- frontend/src/components/Chart.tsx
+++ frontend/src/components/Chart.tsx
@@ -1,15 +1,30 @@
+import { useState, useEffect } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
+import socket from "../utils/Socket";
 
 export default function Chart() {
+  const [time, setTime] = useState<number[]>([]);
+  const [voltage, setVoltage] = useState<number[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = socket.subscribe((data) => {
+      console.log("Received data from backend:", JSON.stringify(data));
+      setTime((prev) => [...prev, data.seq]);
+      setVoltage((prev) => [...prev, data.voltage]);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="w-full h-full bg-gray-800 rounded-lg p-4">
       <h2 className="text-white text-xl mb-4">Chart Placeholder</h2>
       <div className="w-full h-full bg-gray-100 rounded-lg flex items-center justify-center">
         <LineChart
-          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+          xAxis={[{ data: time }]}
           series={[
             {
-              data: [2, 5.5, 2, 8.5, 1.5, 5],
+              data: voltage,
             },
           ]}
           height={300}
