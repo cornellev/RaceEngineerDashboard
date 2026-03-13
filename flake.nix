e3a7004c80ba25ddc6361a2a86ac40360dfe6b16
erics118 <52634785+erics118@users.noreply.github.com>
2026-03-13 19:01:52 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (python314.withPackages (ps: [ ps.pip ]))
+              bun
+            ];
+          };
+        };
+    };
+}
