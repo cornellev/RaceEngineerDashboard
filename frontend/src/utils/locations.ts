ae16ed6f64933409c25108be519dfa53d90e5f1a
Donte Truong <dontetruong@gmail.com>
2026-03-25 20:02:30 -0400

    Location options comment helper

--- frontend/src/utils/locations.ts
+++ frontend/src/utils/locations.ts
@@ -3,6 +3,11 @@ interface Location {
   lng: number;
 }
 
+/**
+ * Location Options
+ * @key "B-Lot"
+ * @key "Indianapolis Motor Speedway"
+ */
 const locations: Record<string, Location> = {
   "B-Lot": { lat: 42.44656683714592, lng: -76.4630167437453 },
   "Indianapolis Motor Speedway": {
